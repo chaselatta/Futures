fc635b944d78714ec256c43e40e9ee30cadbee96
Chase Latta <chaselatta@gmail.com>
2016-07-19 10:55:57 -0700

    Added Await function

---
+++ Futures/Await.swift
@@ -0,0 +1,36 @@
+//
+//  Await.swift
+//  Futures
+//
+//  Created by Chase Latta on 7/18/16.
+//  Copyright © 2016 Chase Latta. All rights reserved.
+//
+
+import Foundation
+
+public struct Await {
+    
+    public static func result<T>(future: Future<T>) throws -> T {
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        var valueMaybe: T? = nil
+        var errorMaybe: ErrorProtocol? = nil
+        
+        future.respond { result in
+            result.withValue { valueMaybe = $0 }
+            result.withError { errorMaybe = $0 }
+            semaphore.signal()
+        }
+        
+        semaphore.wait()
+        
+        if let value = valueMaybe {
+            return value
+        } else if let error = errorMaybe {
+            throw error
+        }
+        
+        fatalError()
+    }
+}
---
+++ FuturesTests/AwaitTests.swift
@@ -0,0 +1,65 @@
+//
+//  AwaitTests.swift
+//  Futures
+//
+//  Created by Chase Latta on 7/18/16.
+//  Copyright © 2016 Chase Latta. All rights reserved.
+//
+
+import XCTest
+
+class AwaitTests: XCTestCase {
+    
+    func testAwaitResult() {
+        let f = Future.value(1)
+        do {
+            let v = try Await.result(future: f)
+            XCTAssertEqual(v, 1)
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    
+    func testAwaitResult_delayed() {
+        let f = Promise.after(when: .now()  .milliseconds(10), value: 1)
+        let exp = expectation(withDescription: "wait for promise")
+        do {
+            let v = try Await.result(future: f)
+            XCTAssertEqual(v, 1)
+            exp.fulfill()
+        } catch {
+            XCTFail()
+        }
+        waitForExpectations(withTimeout: 1, handler: nil)
+    }
+    
+    
+    func testAwaitFail() {
+        let e = NSError(domain: "await", code: 1, userInfo: nil)
+        let f = Future<Any>.error(e)
+        do {
+            let _ = try Await.result(future: f)
+            XCTFail("should throw")
+        } catch {
+            XCTAssertEqual(e, error as NSError)
+        }
+    }
+    
+    
+    func testAwaitFail_delayed() {
+        let e = NSError(domain: "await", code: 1, userInfo: nil)
+        let f = Promise<Any>()
+        
+        DispatchQueue.global().async {
+            f.fail(error: e)
+        }
+        do {
+            let _ = try Await.result(future: f)
+            XCTFail("should throw")
+        } catch {
+            XCTAssertEqual(e, error as NSError)
+        }
+    }
+    
+}
